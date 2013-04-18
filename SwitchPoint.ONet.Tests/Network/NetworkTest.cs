15f8039aa91dad72a1e22a86f4406e1bebc16c56
James Thorne <james@thorne.bz>
2013-04-18 22:20:46 +0100

    added split host in host constructor and tests

---
+++ SwitchPoint.ONet.Tests/Network/NetworkTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SwitchPoint.ONet.Network;
+namespace SwitchPoint.ONet.Tests.Network
+{
+    [TestClass]
+    public class NetworkTest
+    {
+        [TestMethod]
+        public void ShouldSplitHostName()
+        {
+            string Vector = "switchpoint.co.uk:1234";
+            Host Host = new Host(Vector);
+
+            Assert.AreEqual("switchpoint.co.uk", Host.HostName);
+            Assert.AreEqual(1234, Host.Port);
+        }
+
+        [TestMethod]
+        public void ShouldSplitIPv4Address()
+        {
+            string Vector = "172.16.1.1:1234";
+            Host Host = new Host(Vector);
+
+            Assert.AreEqual("172.16.1.1", Host.HostName);
+            Assert.AreEqual(1234, Host.Port);
+        }
+
+        [TestMethod]
+        public void ShouldSplitRFC3986IPv6Address()
+        {
+            string Vector = "[2001:db8::1]:1234";
+            Host Host = new Host(Vector);
+
+            Assert.AreEqual("[2001:db8::1]", Host.HostName);
+            Assert.AreEqual(1234, Host.Port);
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ShouldErrorWithNoPort()
+        {
+            string Vector = "switchpoint.co.uk";
+            Host Host = new Host(Vector);
+
+        }
+
+
+    }
+}
--- SwitchPoint.ONet/Network/Host.cs
+++ SwitchPoint.ONet/Network/Host.cs
@@ -8,12 +8,19 @@ namespace SwitchPoint.ONet.Network
 {
     public class Host
     {
-        public String IP { get; set; }
+        public String HostName { get; set; }
         public int Port { get; set; }
 
 
         public Host(String HostName)
         {
+            int colonIndex = HostName.LastIndexOf(':');
+            if (colonIndex == -1)
+            {
+                throw new ArgumentException("Invalid host:port format");
+            }
+            this.HostName = HostName.Substring(0, colonIndex);
+            Port = int.Parse(HostName.Substring(colonIndex + 1));
         }
 
     }
